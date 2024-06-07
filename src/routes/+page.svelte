c37c740d8b7f9794d3983d833edd0ca740aa5c11
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-06-07 08:15:54 -0500

    feat: add initial design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Searchbar from "../components/search-bar.svelte";
+</script>
+
+<div class="container space-y-4 pt-20">
+	<h1
+		class="bg-gradient-to-b from-white to-white/20 bg-clip-text text-center text-7xl font-bold tracking-tight text-transparent"
+	>
+		Eureka
+	</h1>
+	<Searchbar />
+</div>
